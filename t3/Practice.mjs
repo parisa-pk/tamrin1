4b97065cb47cf33bee2ed8fd6e5386c159249381
parisa <parisapichka1@gmail.com>
2022-11-25 06:49:24 -0800

    file4

--- t3/Practice.mjs
+++ t3/Practice.mjs
@@ -1,15 +1,20 @@
 import  fs  from "fs";
 
+import chalk from "chalk";
+import { log } from "console";
+
 const word = 'hello';
 
 const arr = fs.readdirSync('.'); 
 
 for (let i = 0 ; i < arr.length ; i++) {
     const helloWord = arr[i];
+    if (helloWord === 'node_modules')continue;
+    
     const file = fs.readFileSync(helloWord , 'utf-8')
-    if (file.includes(word) && helloWord !== 'practice.mjs') {
+    if (file.includes(word) && helloWord ==!'practice.mjs') {
         console.log(helloWord);
     }
-    
+    log(chalk.blue(word))
 }
 
