3cb40a3b6ebcce8411819a7cb43a97b4e374537c
parisa <parisapichka1@gmail.com>
2022-12-05 06:13:56 -0800

    bookmark3

--- t6/bookMark/src/App.js
+++ t6/bookMark/src/App.js
@@ -3,15 +3,14 @@ import Post from './Post';
 import {useState , useEffect} from 'react';
 
 
-
 function App() {
 
+  const[post , setPost] = useState([]);
 
-  const[post , setPost] = useState([])
 
   useEffect(() => axios.get("https://jsonplaceholder.typicode.com/posts").then((res)=>{
       setPost(res.data)
-      console.log(res.data);}) , [])
+      console.log(res.data);}) , []);
 
 
   return (
